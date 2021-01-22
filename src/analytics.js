6f9c8e5fa22443ab4f7d32e452ef71a1b0eeb039
zigridar <safonenko.max@yandex.ru>
2021-01-22 21:03:05 +0500

    init app

---
+++ src/analytics.js
@@ -0,0 +1,25 @@
+const createAnalytics = () => {
+    let counter = 0
+
+    let isDestroyed = false
+
+    const listener = () => counter++
+
+    document.addEventListener('click', listener)
+
+    return {
+        destroy() {
+            isDestroyed = true
+            document.removeEventListener('click', listener)
+        },
+
+        getClicks() {
+            if (isDestroyed)
+                return 'Analytic is destoyd'
+
+            return counter
+        }
+    }
+}
+
+const analytic = createAnalytics()
