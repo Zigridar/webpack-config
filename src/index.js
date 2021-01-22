a3476123ff1e9f3f4690418d9e18b5a8de522229
zigridar <safonenko.max@yandex.ru>
2021-01-22 22:17:06 +0500

    json import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Post from './Post'
 import './styles/style.css'
+import json from '../assets/json'
 
 const post = new Post('Webpack post title')
 
 console.log(post.toString())
+console.log(json)
