aa92a7e2fdccbb7de585ac5052110a76200e4716
zigridar <safonenko.max@yandex.ru>
2021-01-22 22:02:42 +0500

    webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 /** Webpack config **/
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
         main: './src/index.js',
         analytics: './src/analytics.js'
     },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
 }
